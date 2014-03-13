8e702c63e3c9e13bfc64ed5df038d764dcf86991
goodbrandcraig <goodbrandcraig@gmail.com>
2014-03-13 12:23:01 +0000

    Tidied up some forms and made buttons look nicer. Also added radio buttons in the review form as it makes more sense than textfields for numbers

--- ITech-Legless-Industries-master/legless_industries_project/the_watering_hole/forms.py
+++ ITech-Legless-Industries-master/legless_industries_project/the_watering_hole/forms.py
@@ -3,10 +3,13 @@ __author__ = 'Craig'
 from django import forms
 from the_watering_hole.models import UserProfile, Bar, Photo, Category, Review
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(help_text="Please enter a username.")
+    email = forms.CharField(help_text="Please enter your email.")
+    password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
 
     class Meta:
         model = User
@@ -14,6 +17,9 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
+    website = forms.URLField(help_text="Please enter your website.", required=False)
+    picture = forms.ImageField(help_text="Select a profile image to upload.", required=False)
+
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
@@ -26,7 +32,34 @@ class BarForm(forms.ModelForm):
         fields = ('name', 'address', 'bio',)
 
 
+class HorizontalRadioRenderer(forms.RadioSelect.renderer):
+    """ this overrides widget method to put radio buttons horizontally
+        instead of vertically.
+    """
+    def render(self):
+            """Outputs radios"""
+            return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
+
+
 class ReviewForm(forms.ModelForm):
+    CHOICES = [(1, '1'),
+               (2, '2'),
+               (3, '3'),
+               (4, '4'),
+               (5, '5')]
+    booze = forms.ChoiceField(choices=CHOICES, label="How did you find the refreshments?",
+                              widget=forms.RadioSelect(renderer=HorizontalRadioRenderer),)
+
+    beats = forms.ChoiceField(choices=CHOICES, label="What about the music?",
+                              widget=forms.RadioSelect(renderer=HorizontalRadioRenderer),)
+
+    barstaff = forms.ChoiceField(choices=CHOICES, label="And the Bar Staff?",
+                                 widget=forms.RadioSelect(renderer=HorizontalRadioRenderer),)
+
+    bucks = forms.ChoiceField(choices=CHOICES, label="Did you get your money's worth?",
+                              widget=forms.RadioSelect(renderer=HorizontalRadioRenderer),)
+
+    text_review = forms.CharField(widget=forms.Textarea, label="Add a comment to your review!")
     class Meta:
         model = Review
         fields = ('booze', 'beats', 'barstaff', 'bucks', 'text_review')
