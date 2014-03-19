316f5d04d1a52687ed6a872e76c59c0f3237e673
goodbrandcraig <goodbrandcraig@gmail.com>
2014-03-19 10:53:38 +0000

    Made the alterations suggested by Leif. Improved the style of the entire site and the layout of the barpage to get rid of the '90s feel.

--- ITech-Legless-Industries-master/legless_industries_project/legless_industries_project/urls.py
+++ ITech-Legless-Industries-master/legless_industries_project/legless_industries_project/urls.py
@@ -12,9 +12,6 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
     url(r'^the_watering_hole/', include('the_watering_hole.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
